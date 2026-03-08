e5a8b3edc907933d0ddf544e91ea2728fd0da1bd
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-08 05:11:17 +0100

    Changes of the day 2026-03-08T05:11:17+01:00

--- src/SimpleCacheProvider.php
+++ src/SimpleCacheProvider.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
- * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
- *
- * @license CC-BY-ND-4.0
- *
- * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
- * @see {@link https://github.com/tomaschochola} GitHub Profile
- * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
- */
-
 declare(strict_types=1);
 
 namespace TomasChochola\Psr\SimpleCache;
@@ -26,14 +15,13 @@ use Traversable;
  *
  * @implements IteratorAggregate<mixed, mixed>
  */
-readonly class SimpleCacheManifest implements IteratorAggregate
+readonly class SimpleCacheProvider implements IteratorAggregate
 {
     #[NoDiscard]
     #[Override]
     public function getIterator(): Traversable
     {
         yield ApcuSimpleCache::class => [ApcuSimpleCache::class, 'unload'];
-
         yield CacheInterface::class => [ApcuSimpleCache::class, 'unload'];
     }
 }
